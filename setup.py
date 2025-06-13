ed6142ba1754dd88fba6e573d8c29b26d08883fc
Yutong Zhang <zhangyt2000@gmail.com>
2025-06-13 13:42:36 -0600

    Fix mesh path and add missing data files

--- setup.py
+++ setup.py
@@ -31,11 +31,7 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test']),
     include_package_data=True,
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tonyzyt2000',
